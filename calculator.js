d7902c81a5ecd535f34e20abbf443a9ad354d9f4
seba3510 <seba3510@protonmail.com>
2024-01-03 10:38:32 -0400

    Improve script documentation as well
    - Provide an overview of the calculator script's functionality
    - Add comments for the 'operator' variable
    - Document constants 'screenRef' and 'btnsRef' with explanations
    - Enhance overall clarity and understanding of the script

--- calculator.js
+++ calculator.js
@@ -1,17 +1,66 @@
+/**
+ * This script implements a basic calculator with addition, subtraction,
+ * multiplication, and division operations.
+ * 
+ * @author Sebastian Corporan Berrios
+ */
+
+
+//=======================================================================================
+
+
+/**
+ * Stores the first number
+ * 
+ * @type {number}   
+ */
+let a = 0;
+
+
+//=======================================================================================
+
+
+
+/**
+ * Stores the second number
+ * 
+ * @type {number}
+ */
+let b = 0;
+
+
+//=======================================================================================
+
+
+
+/**
+ * Stores the operator. This determines the operation to be done with
+ * the 2 numbers (addition, subtraction, multiplication, or division)
+ * 
+ * @type {string}
+ */
+let operator = "";
+
+
+//=======================================================================================
+
+
+
+
+/**
+ * Reference to the screen that displays the output of the operation
+ * 
+ * @type {HTMLElement}
+ */
+const screenRef = document.getElementById("input-screen");
+//=======================================================================================
+
+/**
+ * Reference to all of the buttons of the calculator
+ * 
+ * @type {NodeList}
+ */
 const btnsRef = document.querySelectorAll("button");
 
 
-
-btnsRef.forEach((btn) => {
-
-    btn.addEventListener("click", () => {
-
-        console.log("Button Clicked: " + btn.textContent);
-
-    });
-
-});
-
-
-
-
+//=======================================================================================
